d56f53460b01763a448fd63d428b667d17491c3f
CycMei <chayalikemei@outlook.com>
2016-10-26 21:02:27 +0800

    ee

--- DesignModule/SimpleRemoteControl.cpp
+++ DesignModule/SimpleRemoteControl.cpp
@@ -1,14 +1,14 @@
 #include "SimpleRemoteControl.h"
 #include "Command.h"
 
-SimpleRemoteControl::SimpleRemoteControl(Command *command):_command(command) {
+SimpleRemoteControl::SimpleRemoteControl(std::shared_ptr<Command> command):_command(command) {
 
 }
 SimpleRemoteControl::~SimpleRemoteControl() {
 
 }
 void SimpleRemoteControl::setCommand(Command *command) {
-	_command = command;
+	//_command = command;
 }
 void SimpleRemoteControl::buttonWasPressed() {
 	_command->execute();
--- DesignModule/SimpleRemoteControl.h
+++ DesignModule/SimpleRemoteControl.h
@@ -1,13 +1,14 @@
 #ifndef SIMPLEREMOTECONTROL_H
 #define SIMPLEREMOTECONTROL_H
+#include<memory>
 class Command;
 class SimpleRemoteControl {
 public:
-	explicit SimpleRemoteControl(Command*);
+	explicit SimpleRemoteControl(std::shared_ptr<Command>);
 	~SimpleRemoteControl();
 	void setCommand(Command *command);
 	void buttonWasPressed();
 private:
-	Command *_command;
+	std::shared_ptr<Command> _command;
 };
 #endif // !SIMPLEREMOTECONTROL_H
